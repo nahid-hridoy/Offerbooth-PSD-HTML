44adf4af60423a18482cfd40d7195597468cc05c
nahid-hridoy <nahid_hridoy@outlook.com>
2017-10-03 23:09:21 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,44 @@
-(function($) {
+(function ($) {
     "use strict";
 
-    jQuery(document).ready(function($) {
+    jQuery(document).ready(function ($) {
 
         /*masonary active*/
         // $(".image-list").masonry();
 
+
+        /* $(window).bind("scroll", function () {
+            $(window).height();
+            $(window).scrollTop() > 100 && $(".header-area").addClass("fixed"), $(window).scrollTop() < 111 && $(".header-area").removeClass("fixed")
+        }); */
+
     });
 
 
-    jQuery(window).load(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+
+    $(".responsive-menu-trigger").on("click", function () {
+        $(".off-canvas-menu,.responsive-menu-trigger").addClass("active ");
+        return false;
+    });
+
+    $(".menu-close").on("click", function () {
+        $(".off-canvas-menu,.responsive-menu-trigger").removeClass("active");
+    });
+
+
+    jQuery(window).load(function () {
 
 
     });
